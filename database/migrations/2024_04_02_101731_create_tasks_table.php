f5eaeaa01cd44bcbdb8981ffbce986b99396658e
yessi-bo8 <164057691+yessi-bo8@users.noreply.github.com>
2024-04-09 19:41:35 +0200

    Set up Task model

--- database/migrations/2024_04_02_101731_create_tasks_table.php
+++ database/migrations/2024_04_02_101731_create_tasks_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('project_id');
+            
+            $table->foreign('project_id')->references('id')->on('projects');
         });
     }
 
